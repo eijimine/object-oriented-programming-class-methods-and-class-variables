cc8e7b4aa1d5da9ea99754315711bf6757a0771c
eiji <eijimine@gmail.com>
2017-07-05 20:00:00 -0400

    Zombie Complete

--- part3 - zombie.rb
+++ part3 - zombie.rb
@@ -24,29 +24,42 @@ class Zombie
      end
   end
 
+
   def encounter
-    random = rand(2)
+    random = rand(3)
     if random == 0
       outrun_zombie?
     elsif random == 1
       survive_attack?
     else
       puts "You have caught the plague and turned into a zombie!"
+      zombie = Zombie.new(rand(@@max_speed), rand(@@max_strength))
+      @@horde << zombie
     end
   end
 
   def outrun_zombie?
-
+    spd = rand(@@max_speed)
+    if spd >= @speed
+       puts "You got away!"
+     else
+       "You did not get away..."
+     end
   end
 
   def survive_attack?
-
+    str = rand(@@max_strength)
+    if str >= @strength
+       puts "You survived the attack!"
+     else
+       puts "You did not survive the attack"
+     end
   end
 
   ###### Class Methods
 
   def self.all
-    @@horde.count
+    @@horde
   end
 
   def self.new_day
@@ -80,3 +93,10 @@ class Zombie
 end
 
 Zombie.spawn
+
+zombie1 = Zombie.spawn
+puts Zombie.all
+zombie1[0].encounter
+puts Zombie.all
+zombie1[0].encounter
+puts Zombie.all
